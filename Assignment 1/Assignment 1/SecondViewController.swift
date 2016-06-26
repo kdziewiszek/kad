2a93b1f94db97dea6a8c38e633df495b51d4d433
kdziewiszek <Kama@krzysztof-dziewiszeks-macbook-pro.local>
2016-06-25 21:16:12 -0400

    Assignment 1 - Katie Dziewiszek

---
+++ Assignment 1/Assignment 1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Assignment 1
+//
+//  Created by Krzysztof Dziewiszek on 6/25/16.
+//  Copyright © 2016 Katherine Dziewiszek. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
