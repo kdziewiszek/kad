1cd9d2b02199980cb594fbf39d2f53c053f5e08c
kdziewiszek <Kama@krzysztof-dziewiszeks-macbook-pro.local>
2016-07-30 00:37:37 -0400

    Trying to get prototype to work with my grid view

---
+++ class10/class10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  class10
+//
+//  Created by Krzysztof Dziewiszek on 7/25/16.
+//  Copyright © 2016 Katherine Dziewiszek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ class10/class10Tests/class10Tests.swift
@@ -0,0 +1,36 @@
+//
+//  class10Tests.swift
+//  class10Tests
+//
+//  Created by Krzysztof Dziewiszek on 7/25/16.
+//  Copyright © 2016 Katherine Dziewiszek. All rights reserved.
+//
+
+import XCTest
+@testable import class10
+
+class class10Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
